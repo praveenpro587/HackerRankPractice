4f84be4d65407055f518aafa4e78ee64d42d2ccb
praveenpro587 <42086848+praveenpro587@users.noreply.github.com>
2021-06-25 14:41:28 +0530

    Create Day1.scala

---
+++ Day1.scala
@@ -0,0 +1,15 @@
+object Solution extends App{
+    def main(args: Array[String]) {
+        val i = 4
+        val d = 4.0
+        val s = "HackerRank "
+        
+        val j=scala.io.StdIn.readLine().toInt
+        val c=scala.io.StdIn.readLine().toDouble
+        val str=scala.io.StdIn.readLine().toString
+        
+        println(i+j)
+        println(d+c)
+        println(s+str)
+    }
+}
